5d63b2aa67e8e7619b3a5bbc3038c8062a4d3ba6
tl-kieran-allen <kieran.allen@truelayer.com>
2021-12-01 20:45:59 +0000

    cleanup

--- common/sendMessage.ts
+++ common/sendMessage.ts
@@ -1,5 +1,4 @@
 import { apiClient } from "../services/client";
-import { Constants } from "./constants";
 
 export async function sendMessage(chatId: number, message: string) {
   return await apiClient.post(`/sendMessage`, {
