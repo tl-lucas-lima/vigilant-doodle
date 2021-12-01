de2c28d7824b0f1e4c3793feb59188df367b3586
Lucas Lima <lucas.lima@truelayer.com>
2021-12-01 16:07:53 +0000

    Add models

---
+++ models/TelegramMessageResponse.ts
@@ -0,0 +1,27 @@
+export interface TelegramMessageResponse {
+  update_id: number;
+  message: Message;
+}
+
+export interface Message {
+  message_id: number;
+  from: From;
+  chat: Chat;
+  date: number;
+  text: string;
+}
+
+export interface Chat {
+  id: number;
+  first_name: string;
+  last_name: string;
+  type: string;
+}
+
+export interface From {
+  id: number;
+  is_bot: boolean;
+  first_name: string;
+  last_name: string;
+  language_code: string;
+}
