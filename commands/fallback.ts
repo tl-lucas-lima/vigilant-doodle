a42ca0acede3a080fc88766f505202a09e40375e
tl-kieran-allen <kieran.allen@truelayer.com>
2021-12-02 09:17:36 +0000

    add validation to the register function

--- commands/fallback.ts
+++ commands/fallback.ts
@@ -1,35 +1,55 @@
+import * as Yup from "yup";
 import { RequestBody } from ".";
-import { getAccountNumber } from "../common/getAccountNumber";
+
 import { getChatId } from "../common/getChatId";
 import { getState } from "../common/getState";
 import { sendMessage } from "../common/sendMessage";
 import { setAccountNumber } from "../common/setAccountNumber";
 import { setSortCode } from "../common/setSortCode";
 import { setState } from "../common/setState";
-import { client } from "../main";
-import { Flows } from "../models/Flows";
 import { TelegramMessageResponse } from "../models/TelegramMessageResponse";
+
+const accountNumberValidation = Yup.string().min(8).max(8);
+const sortCodeValidation = Yup.string().min(6).max(6);
 
 export async function fallback(req: RequestBody<TelegramMessageResponse>) {
   const state = await getState(req);
 
   switch (state) {
     case "account_number":
-      await setAccountNumber(req);
-      const ac = getAccountNumber(req);
-      await setState(req, "sort_code");
-      const y = await getState(req);
-      return await sendMessage(
-        getChatId(req),
-        `Please enter your sort-code in the format xx-xx-xx`
-      );
+      return accountNumberValidation
+        .validate(req.body.message.text)
+        .then(async () => {
+          await setAccountNumber(req);
+          await setState(req, "sort_code");
+          return await sendMessage(
+            getChatId(req),
+            `Please enter your 6 digit sort-code`
+          );
+        })
+        .catch(async () => {
+          return await sendMessage(
+            getChatId(req),
+            `Looks like that isn't a valid 8 digit account number, please enter again`
+          );
+        });
     case "sort_code":
-      await setSortCode(req);
-      await setState(req, null);
-      return await sendMessage(
-        getChatId(req),
-        "All done, now enjoy some cardless payments! ( Secured by your boi TrueLayer )"
-      );
+      return sortCodeValidation
+        .validate(req.body.message.text)
+        .then(async () => {
+          await setSortCode(req);
+          await setState(req, null);
+          return await sendMessage(
+            getChatId(req),
+            "All done, now enjoy some cardless payments! ( Secured by your boi TrueLayer )"
+          );
+        })
+        .catch(async () => {
+          return await sendMessage(
+            getChatId(req),
+            "Looks like that isn't  a valid 6 digit sort code, please enter again"
+          );
+        });
     default:
       return await sendMessage(
         getChatId(req),
