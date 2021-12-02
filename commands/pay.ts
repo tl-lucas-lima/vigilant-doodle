005d320c7a96b6b25a22d0e53529e956070cf65a
tl-kieran-allen <kieran.allen@truelayer.com>
2021-12-02 10:37:39 +0000

    remove unused variables

--- commands/pay.ts
+++ commands/pay.ts
@@ -6,9 +6,6 @@ import { sendMessage } from "../common/sendMessage";
 import { TelegramMessageResponse } from "../models/TelegramMessageResponse";
 import { createPayment } from "../services/paymentService";
 
-const hppMockUrl =
-  "https://checkout.t7r.dev/payments-mock#payment_id=123&resource_token=123456&return_uri=https://penny.t7r.dev/redirect";
-
 export async function pay(req: RequestBody<TelegramMessageResponse>) {
   const chatId = getChatId(req);
   const message = req.body.message.text;
@@ -17,10 +14,9 @@ export async function pay(req: RequestBody<TelegramMessageResponse>) {
   const beneficiary = message.match(/(?!@)\w+$/);
 
   if (beneficiary && amount) {
-    // if details exist then we user is registed, otherwise ask them to register
+    // if details exist then the user is registed, otherwise ask them to register
     const deets = await getDeets(req, beneficiary[0]);
 
-    const user = Constants.demoUsers.find((u) => u.username === beneficiary[0]);
     if (!deets.accountNumber || !deets.sortCode) {
       return await sendMessage(chatId, `uh oh. 👀 Looks like @${beneficiary[0]} is missing bank details.`)
     } else {
